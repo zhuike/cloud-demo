e6f7ccdbcc785b89d0c126646b729bb6829308e6
kezhihui <2411596965@qq.com>
2025-07-21 00:12:41 +0800

    版本确认

--- cloud-gateway-demo/src/main/java/org/kezhihui/cloud/demo/gateway/CloudDemoGatewayApplication.java
+++ cloud-gateway-demo/src/main/java/org/kezhihui/cloud/demo/gateway/CloudDemoGatewayApplication.java
@@ -1,10 +1,17 @@
 package org.kezhihui.cloud.demo.gateway;
 
+
 /**
  * @author kezhihui
  * @createTime 2025/06/30 17:11
  */
+
+@SpringBootApplication
 public class CloudDemoGatewayApplication {
+
+    public static void main(String[] args) {
+
+    }
 
 
     
